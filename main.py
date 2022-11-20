a4599eecffdba6e17d189373bae227c664d8c6a6
sulynajimsj <sulynajim2002@gmail.com>
2022-11-20 12:22:43 -0500

    changes

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from deepface import DeepFace
-
-recognition = DeepFace.find(img_path = "MV5BNzA5NGI2ZGMtYzM0Yi00YTFlLWEyNWItYjBlZDZmZWM2OGM3XkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_.jpg", db_path = "faces")
+backends = [
+  'opencv', 
+  'ssd', 
+  'dlib', 
+  'mtcnn', 
+  'retinaface', 
+  'mediapipe'
+]
+recognition = DeepFace.find(img_path = "MV5BNzA5NGI2ZGMtYzM0Yi00YTFlLWEyNWItYjBlZDZmZWM2OGM3XkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_.jpg", db_path = "faces", detector_backend=backends[5])
 print(recognition)
